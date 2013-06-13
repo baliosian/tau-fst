0874d941b8055cdbcf267510eb38b6244c403be4
baliosian <baliosian@fing.edu.uy>
2013-06-13 19:02:58 -0300

    arregle el test de sacar epsilons.

--- src/uy/edu/fing/mina/fsa/test/TestEpsilonRem.java
+++ src/uy/edu/fing/mina/fsa/test/TestEpsilonRem.java
@@ -8,6 +8,7 @@ package uy.edu.fing.mina.fsa.test;
 
 import uy.edu.fing.mina.fsa.tf.SimpleTf;
 import uy.edu.fing.mina.fsa.tf.TfI;
+import uy.edu.fing.mina.fsa.tf.TfString;
 import uy.edu.fing.mina.fsa.tffst.State;
 import uy.edu.fing.mina.fsa.tffst.Tffst;
 import uy.edu.fing.mina.fsa.tffst.Transition;
@@ -24,53 +25,49 @@ public class TestEpsilonRem {
     * 
     * @param args
     */
-//   public static void main(String[] args) {
-//      
-//      TestEpsilonRem test = new TestEpsilonRem();
-//      Tffst.setMinimizeAlways(false);
-//      
-//      //tffst1
-//      Tffst tffst1 = new Tffst();
-//
-//      State s0 = new State();
-//      tffst1.setInitialState(s0);
-//      State s1 = new State();
-//      State s2 = new State();
-//      s2.setAccept(true);
-//
-//      SimpleTf tfd = new SimpleTf();
-//      tfd.setSLabel("D");
-//      tfd.setEventName("d");
-//      Transition trans1 = new Transition(tfd, tfd, s1, 1);
-//      s0.addTransition(trans1);
-//      
-//      SimpleTf tfall = new SimpleTf();
-//      tfall.setAcceptAll();
-//      SimpleTf tfc = new SimpleTf();
-//      tfc.setSLabel("C");
-//      tfc.setEventName("c");
-//      TfI tfall_c = tfall.and(tfc.not());
-//      
-//      Transition trans2 = new Transition(tfall_c, tfall_c, s1, 1);
-//      s1.addTransition(trans2);
-//      
-//      SimpleTf tfk = new SimpleTf();
-//      tfk.setSLabel("K");
-//      tfk.setEventName("k");
-//      SubEpoch sec = new SubEpoch(tfc);
-//      SubEpoch seck = new SubEpoch(tfc);
-//      seck.add(tfk);
-//      Transition trans3 = new Transition(sec, seck, s2, 1);
-//      s1.addTransition(trans3);
-//  
-//      tffst1 = tffst1.toSimpleTransitions();
-//      Utils.showDot(tffst1.toDot("tffst1"));
-//      
-//      tffst1.epsilonRemoval();
-//      Utils.showDot(tffst1.toDot("tffst1 without epsilons"));
-//
-//      tffst1.removeDeadTransitions();
-//      Utils.showDot(tffst1.toDot("tffst1 without dead transitions"));
-//   }
+   public static void main(String[] args) {
+      
+      Tffst.setMinimizeAlways(false);
+      
+      //tffst1
+      Tffst tffst1 = new Tffst();
+
+      State s0 = new State();
+      tffst1.setInitialState(s0);
+      State s1 = new State();
+      State s2 = new State();
+      s2.setAccept(true);
+
+      SimpleTf tfd = new SimpleTf();
+      tfd.setSLabel("D");
+      Transition trans1 = new Transition(tfd, tfd, s1, 1);
+      s0.addTransition(trans1);
+      
+      SimpleTf tfall = new SimpleTf();
+      tfall.setAcceptAll();
+      SimpleTf tfc = new SimpleTf();
+      tfc.setSLabel("C");
+      TfI tfall_c = tfall.and(tfc.not());
+      
+      Transition trans2 = new Transition(tfall_c, tfall_c, s1, 1);
+      s1.addTransition(trans2);
+      
+      SimpleTf tfk = new SimpleTf();
+      tfk.setSLabel("K");
+      TfString sec = new TfString(tfc);
+      TfString seck = new TfString(tfc);
+      seck.add(tfk);
+      Transition trans3 = new Transition(sec, seck, s2);
+      s1.addTransition(trans3);
+  
+      tffst1 = tffst1.toSimpleTransitions();
+      Utils.showDot(tffst1.toDot("tffst1"));
+      
+      tffst1.epsilonRemoval();
+      Utils.showDot(tffst1.toDot("tffst1 without epsilons"));
+
+      tffst1.removeDeadTransitions();
+      Utils.showDot(tffst1.toDot("tffst1 without dead transitions"));
+   }
   
 }
