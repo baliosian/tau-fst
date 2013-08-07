352ea28c6d1fd49e854f3b06ac8f73ae04b0b043
baliosian <baliosian@fing.edu.uy>
2013-08-07 17:19:59 -0300

    Signed-off-by: baliosian <baliosian@fing.edu.uy>

---
+++ src/uy/edu/fing/mina/fsa/test/DoNotHalts.java
@@ -0,0 +1,67 @@
+/*
+ * Created on 12-Aug-2004
+ *
+ * Copyright (C) 2004 Javier Baliosian
+ * All rights reserved.
+ * */
+package uy.edu.fing.mina.fsa.test;
+
+import uy.edu.fing.mina.fsa.tf.SimpleTf;
+import uy.edu.fing.mina.fsa.tffst.State;
+import uy.edu.fing.mina.fsa.tffst.Tffst;
+import uy.edu.fing.mina.fsa.tffst.Transition;
+import uy.edu.fing.mina.fsa.utils.Utils;
+
+/**
+ * @author Javier Baliosian &lt; <a
+ *         href="mailto:jbaliosian@tsc.upc.es">jbaliosian@tsc.upc.es </a>&gt;
+ */
+public class DoNotHalts {
+
+   /**
+    * composition test 1  
+    * 
+    * @param args
+    */
+   public static void main(String[] args) {
+      
+      Tffst.setMinimizeAlways(false);
+      
+     
+      Tffst tffst1 = new Tffst();
+
+      State s0 = new State();
+      tffst1.setInitialState(s0);
+      State s1 = new State();
+      State s2 = new State();
+      State s3 = new State();
+      s3.setAccept(true);
+
+
+//      s0.addTransition(new Transition((new SimpleTf("D")).not(), (new SimpleTf("C")).not(), s0));
+//      s0.addTransition(new Transition((new SimpleTf("D")).not(), SimpleTf.Epsilon(), s0));
+      s0.addTransition(new Transition((new SimpleTf("D")).not(),(new SimpleTf("D")).not(), s0));
+
+      s0.addTransition(new Transition((new SimpleTf("D")).not(), new SimpleTf("D"), s1));
+      
+//      s1.addTransition(new Transition((new SimpleTf("D")).not(), SimpleTf.Epsilon(), s1));
+      s1.addTransition(new Transition((new SimpleTf("D")).not(),(new SimpleTf("D")).not(), s1, 1));
+      s1.addTransition(new Transition((new SimpleTf("D")).not(), SimpleTf.Epsilon(), s3));
+      
+      
+      Utils.showDot(tffst1.toDot("tffst1"));
+      
+      
+      tffst1.setDeterministic(false);
+      tffst1.determinize();    //FIXME  
+      
+      Utils.showDot(tffst1.toDot("determinized tffst1"));  
+      
+      
+
+   }
+  
+
+   
+   
+}
---
+++ src/uy/edu/fing/mina/fsa/test/RateAndPower_1.java
@@ -0,0 +1,28 @@
+package uy.edu.fing.mina.fsa.test;
+
+public class RateAndPower_1 {
+
+  
+  
+  public static void main(String[] args) {
+
+    
+//                Inputs                 |       Outputs
+// -------------------------------------------------------------   
+//    Loss     | Rate       | Power      |  Rate    |  Power
+//    not low  |            | high       | decrease |   keep
+//    not low  |            | not high   | keep     |  increase
+//    low      |            | increase   | keep     |
+    
+    
+    
+    
+    
+  }
+  
+  
+}
+
+
+
+
